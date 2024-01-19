4a05c6424b9fe7e298198dd538fbe1ca0165d148
changhwan hwang <ch.hwang@ecubelabs.com>
2024-01-19 17:00:27 +0900

    feat: tailwind, shadcn

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { cn } from 'lib/utils';
+import { ThemeProvider } from '@components/ThemeProvider';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: 'TypeScript Type System Interaction',
@@ -11,8 +13,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
+import { Header } from '@components/Header';
+import { ThemeButton } from '@components/ThemeButton';
+
 function HomeScreen() {
-  return <div>hello world!</div>;
+  return (
+    <div>
+      <div className="w-100 h-100 bg-zinc-950 dark:bg-white">hello</div>
+      <ThemeButton />
+    </div>
+  );
 }
 
 export default HomeScreen;
